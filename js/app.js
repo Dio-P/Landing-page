1dab23ad0802a506353dc5537443d6b9862bd4bb
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2021-10-23 13:33:56 +0100

    style: all scss have now been turned into css and are working as expected

--- js/app.js
+++ js/app.js
@@ -1,147 +1,145 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
- */
+let linkIdBox= [];
+let getLInkByIdBox= [];
+let sectionBox = [];
+let sectName = [];
+let secLinkCorrs = {};
 
-/**
- * Define arrays and the object that will be used to make the connection between the sections and the nav links
- * 
- */
-
- let helpLink = [];
- let linkMain = [];
- let sectionBoxMain = [];
- let helpSect = [];
- let secLinkConnect = {};
-
-/*I was experimenting to see how I could populate sections dynamicaly. This is in addition to the allready existing sections. 
-If one would want to use this for doing it dynamicaly fro the start, would only need to change a bit the numbers.*/
- function addSectionsDynam(numOfSecToAdd){
-     const frag= document.createDocumentFragment();
-     const addToMain = document.getElementById("main");
-
-    for(let sec= 0; sec < numOfSecToAdd; sec++) {
-        let section = document.createElement("section");
-        section.id = "section" + (5 + sec);
-        section.p = "bla bla bla";
-        section.innerHTML = '<div class="landing__container">' + 
-        '<h2>Section '+ (5 + sec) +'</h2>' +
-        '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>'
-
-        + '<p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>' +
-        '</div>';
-        
-        frag.appendChild(section);
-    }
-    addToMain.appendChild(frag);
- }
- 
- 
- /*
+/*
  *Funtion that counts sections and creates nav menu elements equal to the section numbers
  *this functions gives the id as populating them that is stored in an array that will be used latter
  to create connection with the id of the link button.
  */
- 
- function selectElementById(idName) {
-     return document.getElementById(idName);
- }
- 
- 
- function count_sections(callback) {
-     let sections = document.getElementsByTagName("section");
-     const MyDocFrag = document.createDocumentFragment();
-     const navbar_list = document.getElementById("navbar__list");
-     let r = 0;
- 
-     for (section of sections) {
-         r += 1;
-         let li = document.createElement("LI");
-         li.setAttribute("id", "li_" + r);
-         let link = document.createElement("a");
-         let SecName = "section" + r;
-         let linkId = "a" + r;
-         link.id = linkId;
-         sectionBoxMain.push(selectElementById(SecName));
-         helpSect.push(SecName);
-         link.innerText = "Section " + r;
-         link.setAttribute("class", "menu__link");
-         helpLink.push(linkId);
+
+function selectElementById(idName) {
+    return document.getElementById(idName);
+}
+
+function count_sections() {
+    //this function is counting the sections we have on the HTML. 
+    // Acording to the result of this the menu bars will be populated.
+    console.log("count_sections running");/////////////////////////////////////////////////////////////////
+    let sections = document.getElementsByTagName("section");
+    // creating a document fragment to help with the speed and make it lighter. 
+    // The whole structure is first goint to be appeded here.
+    const MyDocFrag = document.createDocumentFragment();
+    //getting the navdar_list
+    const navbar_list = document.getElementById("navbar__list");
+    let r = 0;
+
+    for (let element of sections){
+        // r Is beinh used as a counter, to add numbers next to the new li elements
+        r += 1;
+        //it's time the function runs (number of sections) a new LI element is being created
+        let li= document.createElement("LI");
+        // this is given an id 
+        li.setAttribute("id", "li_" + r);
+        //it is also given an <a> element on which moste of the rest is going to happen. 
+        let link = document.createElement("a");
+        // the SecName is gong to be used to link it with the section
+        let SecName = "section" + r;
+        // using the r to create another correlation price the linkID
+        let linkId= "a" + r;
+        // giving the link an id
+        link.id= linkId;
+        // selectElementById(SecName) is pushed as single element, for each different section, in sectionBox to be linked latter on with the link id
+        // so to create the condition `if section on view port the correlating link is getting selected by id`.
+        sectionBox.push(selectElementById(SecName));
+        // SecName is being pushed to the sectName array to be used latter
+        sectName.push(SecName);
+        // creating the link text
+        link.innerText = "Project " + r;
+        // giving the link a class
+        link.setAttribute("class", "menu__link")
+        // link Id is pushed in linkIdBox to be used later on, with the observer and getLinkId()
+        linkIdBox.push(linkId);
         /*I found it easier to add the even listener here and leave the rest of the code (observer) as it was. Hope it's alright*/
-         link.addEventListener("click", function(event) {
-             document.getElementById(SecName).scrollIntoView ({behavior: "smooth", block: "end", inline: "nearest"});
-             event.preventDefault();
-         }, false);
- 
-         /*link.href= '#' + SecName;*/
-         li.appendChild(link);
-         MyDocFrag.appendChild(li);
- 
-     }
-     navbar_list.appendChild(MyDocFrag);
- 
- }
- 
- 
- /*start function that creates the object for the connection of the nav items with the sections*/
- 
- function getLinkId(callback) {
-     a = -1;
- 
-     function selectLink(link) {
-         return document.getElementById(link);
-     }
-     for (link in helpLink) {
-         a += 1;
-         let aLink = selectLink(helpLink[a])
-         linkMain.push(aLink);
-         let corrLink = linkMain[a];
-         secLinkConnect[helpSect[a]] = aLink;
-     }
- 
- }
- 
- 
- /*observer*/
- 
- const observOpt = {
-     root: null,
-     rootMargin: "0px",
-     threshold: 0.6,
- };
- 
- /*observer function that checks if section in viewport + gives and takes the .active class*/
- 
- function observCallback(entries, observer) {
-     (entries.forEach((entry) => {
-         if (entry.isIntersecting) {
-             const secLinkCorr = secLinkConnect[entry.target.id];
-             secLinkCorr.classList.add("active");
-             Object.values(secLinkConnect).forEach((item) => {
-                 if (item != secLinkCorr) {
-                     item.classList.remove("active");
-                 }
-             });
-         }
-     }));
- }
- 
- 
- (function () {
-    addSectionsDynam(5);
-    setTimeout(count_sections(addSectionsDynam), 0);
-    setTimeout(getLinkId(count_sections), 0);
-    const observer = new IntersectionObserver(observCallback, observOpt);
-    sectionBoxMain.forEach((sec) => observer.observe(sec));
- 
- })();
+        link.addEventListener("click", function(event) {
+            document.getElementById(SecName).scrollIntoView ({behavior: "smooth", block: "end", inline: "nearest"});
+            event.preventDefault();
+        }, false);
+        // appending the link to the li
+        li.appendChild(link);
+        // appeding the li to the created Doc Frag
+        MyDocFrag.appendChild(li);
+
+    }
+    // appending all the li, which are on the doc Frag, on the navbar_list element to create the menu
+    navbar_list.appendChild(MyDocFrag);
+
+    console.log("sectionBox=>", sectionBox); /////////////////////
+    console.log("linkIdBox=>", linkIdBox); ////////////////
+
+}
+
+function getLinkId(callback) {
+    let a = -1
+    // this is a similar function to the one found on the one above. For each link id in linkIdBox 
+    // we get instead an element of document.getElementById(link) which is later pushed in a new array
+    function selectLink(link) {
+    return document.getElementById(link);
+    }
+    for (let link in linkIdBox) {
+        a += 1;
+        // one by one the elements within testLink array (a1, a2, a3) are chosen and ...
+        let aLink = selectLink(linkIdBox[a])
+        // ...pushed in getLInkByIdBox array as document.getElementById(link);
+        getLInkByIdBox.push(aLink);
+        console.log("a=>", a);//////////////////
+        // Here is where the magic happens and we get on a different array the name(ID) of the section
+        // correlated with the document.getElementById(link) element, so, if the sections is going to be
+        // in the view port, the link id is going to be chosen.
+        secLinkCorrs[sectName[a]] = aLink; 
+    }
+
+    console.log("secLinkCorrs in sec func =>", secLinkCorrs);
+    console.log("sectionBox in sec func =>", sectionBox);
+    console.log("getLInkByIdBox in sec func =>", getLInkByIdBox);
+   
+}
+
+// Observer's options 
+const observOpt = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.6,
+};
+
+// the final function where the observer is built initiated
+function observCallback(entries, observer) {
+    // for each one of the dom etries
+    (entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            // the id of the intersecting entry's link correlation is found in secLinkCorrs and named
+            const secLinkCorr = secLinkCorrs[entry.target.id];
+            console.log("entry.target.id =>", entry.target.id);///////////////////////
+            let viewSection = document.getElementById(entry.target.id)
+            console.log("secLinkCorr =>", secLinkCorr);/////////////////////////
+            // then is being added an active scss class
+            secLinkCorr.classList.add("active");
+            viewSection.classList.add("activeSection");
+            // If the other entries of secLinkCorrs are not the target, they are deprived of the same class
+            Object.values(secLinkCorrs).forEach((item)=>{
+                if (item != secLinkCorr) {
+                    item.classList.remove("active");
+                }
+            });
+            
+            Object.values(sectionBox).forEach((item)=>{
+                if (item != viewSection) {
+                    item.classList.remove("activeSection");
+                }
+            });
+        }
+    }));
+}
+
+// our functions are called
+function run(){
+    count_sections();
+    setTimeout(getLinkId(count_sections),0);
+    // the observer is initiated
+    const observer = new IntersectionObserver (observCallback, observOpt);
+    sectionBox.forEach((sec) => observer.observe(sec));
+}
+
+run()
